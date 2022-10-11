71b821b63143c1c1111659569ec8fb0f89d35a2c
JonnyNiks <jonnyniks@gmail.com>
2022-10-11 10:27:49 +0300

    add materials for lesson 34

---
+++ lesson_34/lesson.ts
@@ -0,0 +1,52 @@
+// Протипизировать данные
+
+const users = [
+  {
+    "id": 7,
+    "email": "michael.lawson@reqres.in",
+    "first_name": "Michael",
+    "last_name": "Lawson",
+    "avatar": "https://reqres.in/img/faces/7-image.jpg",
+    "age": 23,
+    "key": null
+  },
+  {
+    "id": 8,
+    "email": "lindsay.ferguson@reqres.in",
+    "first_name": "Lindsay",
+    "last_name": "Ferguson",
+    "avatar": "https://reqres.in/img/faces/8-image.jpg",
+    "age": 20,
+    "key": null,
+  }
+]
+
+// currency описать с помощью литеральных типов и объединения
+const products = [
+  {
+    id: 1,
+    name: "Burger Premium",
+    price: 6,
+    currency: "euro",
+    ingredients: [["flour", "beef"], ["salad", "cheese", "sauce"]],
+    type: "burger",
+  },
+  {
+    id: 2,
+    name: "Burger Lite",
+    price: 2.3,
+    currency: "euro",
+    ingredients: [["flour", "beef"], ["cheese", "sauce", "cucumber"]],
+    type: "burger",
+    available: true,
+  },
+  {
+    id: 3,
+    name: "Burger Spicy",
+    price: 5,
+    currency: "dollar",
+    ingredients: [["flour", "beef"], ["cheese", "sauce", "pepper"]],
+    type: "burger",
+    available: true
+  },
+]
